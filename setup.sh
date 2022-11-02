71dc428cc8b7fb7fe7234454b21445bdfdc6d98d
Abdullah <89657882+abdullah037@users.noreply.github.com>
2022-11-02 20:44:47 +0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt update -y
+curl -fsSL https://test.docker.com -o test-docker.sh
+sudo sh test-docker.sh
+sudo apt update
+sudo apt install docker-compose -y
+sudo git clone https://github.com/abdullah037/magento2.4.3-php7.4-docker.git
+cd magento2.4.3-php7.4-docker/
+sudo mv magento2-2.4.3 ..
+sudo mv docker-compose.yml ..
+sudo rm -rf magento2.4.3-php7.4-docker
